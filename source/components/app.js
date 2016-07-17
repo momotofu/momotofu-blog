98dfd695b33bff312836499fbbb17b3a37fa6da3
Christopher Reece <mrkita.reece@gmail.com>
2016-07-17 11:12:34 -0600

    reformatted blogpost to include labels and copy

--- source/components/app.js
+++ source/components/app.js
@@ -2,18 +2,23 @@ import React, { Component } from 'react'
 import BlogPost from './blog-post/index.js'
 require('./root-styles/index.styl')
 
-var blogPosts = require('../../api/blog-posts.json')
-console.log("blogPosts from app:", blogPosts)
-
 export default class App extends Component {
   constructor(props) {
     super(props);
+
+    this.BLOGPOSTDATA = require('../../api/blog-posts.json')
+    this.blogPosts = this.BLOGPOSTDATA.map((POSTDATA) => {
+      return (
+        <BlogPost POSTDATA={POSTDATA} key={POSTDATA.meta.UUID} />
+      );
+    });
+
   }
 
   render() {
     return (
       <div>
-        <BlogPost blogPosts={this.blogPosts} />
+        {this.blogPosts}
       </div>
     );
   }
--- source/components/blog-post/index.js
+++ source/components/blog-post/index.js
@@ -6,18 +6,12 @@ require('./index.styl');
 export default class BlogPost extends React.Component {
   constructor(props) {
     super(props);
+    console.log('POSTDATA', this.props.POSTDATA.abstract[0]);
   }
   render() {
     return (
       <div className="BlogPost-container">
-        <Section
-          headingCopy={"Abstact"}
-          bodyCopy={"Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Vestibulum id ligula porta felis euismod semper."}
-         />
-        <Section
-          headingCopy={"#2"}
-          bodyCopy={"Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Vestibulum id ligula porta felis euismod semper."}
-         />
+        <Section bodyCopy={this.props.POSTDATA.abstract} />
       </div>
     );
   }
