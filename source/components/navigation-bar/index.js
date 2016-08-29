01a77503b8c02609e676e5437bd579af455abbbc
Christopher Reece <mrkita.reece@gmail.com>
2016-08-28 22:47:56 -0600

    set up basic nav sort

--- source/components/navigation-bar/index.js
+++ source/components/navigation-bar/index.js
@@ -3,25 +3,65 @@ import { Link } from 'react-router'
 require('./index.styl')
 var classNames = require('classnames')
 
-class NavigationBar extends React.Component {
-  constructor(props) {
-    super(props)
+var NavigationBar = React.createClass({
+  componentWillMount: function() {
+    var links = ['/', 'blog', 'about', 'contributions', 'contact']
+    this.setState({
+      links: links.map((value, i) => {
+        var label = ''
+        if (value == '/')
+          label = 'Momotofu'
+        else
+          label = value
+        return (
+           <Link
+            to={value}
+            activeClassName='NavigationBar-signifier-active'
+            className='NavigationBar-signifier'
+            key={value}
+            onClick={this.sortLinks.bind(null, value)}>
+            {label}
+          </Link>
+        )
+      })
+    })
+  },
+  removeArrayItem: function(array, index) {
+    return array.slice(0, index)
+      .concat(array.slice(index + 1))
+  },
+  sortLinks: function(linkId) {
+    var links = this.state.links
+    var newLinks
+    var activeLinkIndex
+    var activeLink
 
-  }
-  render() {
-    return(
+    links.forEach((n, i) => {
+      if (n.key === linkId)
+        activeLinkIndex = i
+    })
+    if (activeLinkIndex == 0)
+      return
+
+    activeLink = links[activeLinkIndex]
+    links = this.removeArrayItem(links, activeLinkIndex)
+    links.unshift(activeLink)
+    console.log('links: ', links)
+    this.setState({
+      links: links
+    })
+  },
+  render: function() {
+    return (
       <div className='NavigationBar-spacer'>
-        <div className='NavigationBar-container'>
-          <Link to="/" className='NavigationBar-signifier'>Momotofu</Link>
-          <Link to="/blog" className='NavigationBar-signifier'>Blog</Link>
-          <Link to="/about" className='NavigationBar-signifier'>About</Link>
-          <Link to="/contributions" className='NavigationBar-signifier'>Contributions</Link>
-          <Link to="/contact" className='NavigationBar-signifier'>Contact</Link>
+        <div className='NavigationBar-container' ref='nav'>
+          {this.state.links}
           {/*<h1 className='NavigationBar-signifier'>{this.props.METADATA.title}</h1>*/}
         </div>
       </div>
     )
   }
-}
+})
 
 export default NavigationBar
+
