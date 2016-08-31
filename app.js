95362afe904354b2d17140cf19390479c01d5903
Christopher Reece <mrkita.reece@gmail.com>
2016-08-30 22:52:18 -0600

    set up private email

--- app.js
+++ app.js
@@ -6,6 +6,27 @@ var path = require('path')
 var engines = require('consolidate')
 var Router = require('./router')
 var bodyParser = require('body-parser')
+var nodemailer = require('nodemailer')
+
+/**
+  Configure email transporter
+  */
+
+var smtpConfig = {
+  host: process.env.EMAIL_HOST,
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_TOKEN
+  }
+}
+
+var emailTransporter = nodemailer.createTransport(smtpConfig)
+
+/**
+  Configure app
+  */
 
 app.engine('jade', engines.jade)
 
@@ -16,16 +37,33 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/js', express.static(__dirname + '/bin'))
 app.use('/:page', Router)
 
+/**
+  Handle routes
+  */
 
 app.get('/', function(req, res) {
   res.redirect('/momotofu');
 })
 
 app.post('/sendForm', function(req, res) {
-  console.log('form request', req.body)
-  res.status(200).send('success')
+  console.log('req.body', req.body.email)
+  emailTransporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to: process.env.EMAIL_USER,
+    subject: 'New message from: ' + req.body.name + ' with email: ' + req.body.email,
+    text: req.body.message
+  }, function(err, info) {
+    if (err) {
+      console.log(err)
+      res.status(500).send(err)
+    } else {
+      console.log('Message sent: ', info.response)
+      console.log('info: ', info)
+      res.status(200).send('success')
+    }
+  })
 })
 
-var server = app.listen(process.env.PORT || 8000, function() {
+var server = app.listen(process.env.PORT || 9000, function() {
   console.log('Server running at http://localhost:' + server.address().port)
 })
