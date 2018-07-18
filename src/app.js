eecc7fbc60c6efd31815fed4b16c03660670d4e3
Christopher Reece <mrkita.reece@gmail.com>
2018-07-18 10:03:11 +0900

    deleted old code

--- src/app.js
+++ src/app.js
@@ -1,45 +1,21 @@
-import React from 'react'
-import { Router, Route, Link, IndexRoute, browserHistory, Redirect, hashHistory} from 'react-router'
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import MomotofuPage from './pages/momotofu-page'
-import ContributionsPage from './pages/contributions-page'
-import ContactPage from './pages/contact-page'
-import AppContainer from './components/app-container'
-import BlogPosts from './components/blog-posts'
-import '../utils/hyphenate.js'
-
-require('./root-styles/index.styl')
-
-// const store = createStore(reducer)
-
-var App = React.createClass({
-  getInitialState: function() {
-    return {routerState: {prevState: '', nextState: ''}}
-  },
-  getChildContext: function() {
-    return {routerState: this.state.routerState}
-  },
-  update: function(prevState, nextState) {
-    this.setState({
-      routerState: { prevState: prevState.location.pathname, nextState: nextState.location.pathname}
-    })
-  },
-  render: function() {
+class App extends Component {
+  render() {
     return (
-      <Router history={ browserHistory }>
-        <Route onChange={ this.update } path='/' component={ AppContainer }>
-          <Route path='/momotofu' component={ MomotofuPage }/>
-          <Route path='/blog' component={ BlogPosts }/>
-          <Route path='/contributions' component={ ContributionsPage }/>
-          <Route path='/contact' component={ ContactPage }/>
-        </Route>
-      </Router>
-    )
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
   }
-})
-
-App.childContextTypes = {
-  routerState: React.PropTypes.object
 }
 
-export default App
+export default App;
