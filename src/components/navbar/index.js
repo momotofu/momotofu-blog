1d1d517b6c4627d2dcd9b328d4b96fcf814b2f94
Christopher Reece <mrkita.reece@gmail.com>
2018-07-25 20:57:25 +0900

    abstracted BrowserRouter to top of app

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {
-  BrowserRouter as Router,
   Route,
   Link
 } from 'react-router-dom'
@@ -10,19 +9,17 @@ import './index.css'
 class NavBar extends React.Component {
   render() {
     return (
-      <Router>
-        <div>
-          <ul>
-            <li><Link to="/">About</Link></li>
-            <li><Link to="/blog">Blog</Link></li>
-            <li><Link to="/contributions">Contributions</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-          </ul>
-          <Route exact path="/" />
-          <Route path="/about" />
-          <Route path="/topics" />
-        </div>
-      </Router>
+      <div>
+        <ul>
+          <li><Link to="/">About</Link></li>
+          <li><Link to="/blog">Blog</Link></li>
+          <li><Link to="/contributions">Contributions</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+        <Route exact path="/" />
+        <Route path="/about" />
+        <Route path="/topics" />
+      </div>
     )
   }
 }
