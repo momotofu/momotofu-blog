087f378d017d2b6042d46714aac9688fc3beb7c7
Christopher Reece <mrkita.reece@gmail.com>
2018-07-28 10:06:55 +0900

    removed container-fluid wrapper from nav

--- src/App.js
+++ src/App.js
@@ -7,11 +7,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        <div className="container-fluid">
-          <NavBar />
-        </div>
+        <NavBar />
         <div className="container">
-          <p>hello</p>
         </div>
       </div>
     )
