3de8eae074d98cfb308bd6676601518e9c550415
Christopher Reece <mrkita.reece@gmail.com>
2018-08-07 11:35:11 +0900

    disabled horizontal scrolling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import './App.scss'
+import './App.css'
 import NavBar from './components/navbar'
 import 'bootstrap/dist/css/bootstrap-grid.min.css'
 import 'bootstrap/dist/css/bootstrap-reboot.min.css'
