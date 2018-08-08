4c26bac99dd934ea456139ea57136041f9d256cb
Christopher Reece <mrkita.reece@gmail.com>
2018-08-08 17:13:57 +0900

    installed scrollmagic

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,13 +1,44 @@
 import React from 'react'
-
-import './index.css'
 import SakuraTree from '../../components/sakuraTree'
+import './index.css'
 
 class AboutPage extends React.Component {
+  componentDidMount() {
+    const Linear = window.Linear
+    const ScrollMagic = window.ScrollMagic
+    const SakuraTree = document.getElementById('#SakuraTree')
+    const Test = document.getElementById('#Test')
+
+    this.controller = new ScrollMagic.Controller({
+      globalSceneOptions: {
+        triggerHook: 'onEnter',
+        duration: '200%'
+      }
+    })
+
+
+    new ScrollMagic.Scene({ triggerElement: SakuraTree })
+      .setTween('#SakuraTree > section', {y: '80%', ease: Linear.easeNone})
+      .addTo(this.controller)
+
+    new ScrollMagic.Scene({ triggerElement: Test })
+      .setTween('#Test > section', {y: '80%', ease: Linear.easeNone})
+      .addTo(this.controller)
+
+  }
+
   render() {
     return (
       <div className="AboutPage">
-        <SakuraTree />
+        <div id="#SakuraTree">
+          <SakuraTree />
+        </div>
+        <div style={{ width: '100%', background: 'blue', height: '200px'}}>
+        </div>
+        <div id="#Test">
+          <section style={{ width: '100%', background: 'pink', height: '700px'}}>
+          </section>
+        </div>
       </div>
     )
   }
