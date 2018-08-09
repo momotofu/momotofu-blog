d84840728487b08f541535e7c1983bcf0970b5e0
Christopher Reece <mrkita.reece@gmail.com>
2018-08-09 12:24:51 +0900

    added animated text

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -6,7 +6,33 @@ class AboutPage extends React.Component {
   componentDidMount() {
     const Linear = window.Linear
     const ScrollMagic = window.ScrollMagic
+    const anime = window.anime
+    const $ = window.$
 
+    // Wrap every letter in a span
+    anime.timeline({loop: false})
+      .add({
+        targets: '#greeting .letter',
+        opacity: 1,
+        translateY: [-40,0],
+        easing: "easeOutExpo",
+        duration: 1400,
+        delay: function(el, i) {
+          return 100 * i;
+        }
+      })
+      .add({
+        targets: '#greeting-name .letter',
+        opacity: 1,
+        translateY: [-40,0],
+        easing: "easeOutExpo",
+        duration: 1200,
+        delay: function(el, i) {
+          return 50 * i;
+        }
+      })
+
+    // Scrollmagic start
     this.parallaxController = new ScrollMagic.Controller({
       globalSceneOptions: {
         triggerHook: 'onEnter',
@@ -29,12 +55,40 @@ class AboutPage extends React.Component {
       .setPin(document.getElementById('#pin2'))
       .addIndicators({name: '1 (duration: 300)'})
       .addTo(this.pinController)
+    // Scrollmagic end
 
   }
 
   render() {
     return (
       <div className="AboutPage">
+        <div className="greeting-hero">
+          <h1 className="heading" id="greeting">
+            <span className="letter">H</span>
+            <span className="letter">e</span>
+            <span className="letter">l</span>
+            <span className="letter">l</span>
+            <span className="letter">o</span>
+            <span className="letter">,</span>
+          </h1>
+          <h1 className="heading" id="greeting-name">
+            <span className="letter">I</span>
+            <span className="letter">'</span>
+            <span className="letter">m</span>
+            {' '}
+            <span className="letter">C</span>
+            <span className="letter">h</span>
+            <span className="letter">r</span>
+            <span className="letter">i</span>
+            <span className="letter">s</span>
+            <span className="letter">t</span>
+            <span className="letter">o</span>
+            <span className="letter">p</span>
+            <span className="letter">h</span>
+            <span className="letter">e</span>
+            <span className="letter">r</span>
+          </h1>
+        </div>
         <div id="#SakuraTree">
           <SakuraTree />
         </div>
