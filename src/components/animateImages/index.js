ad9e46d2b83d1eacc43b28ed76a656fd6716d9e9
Christopher Reece <mrkita.reece@gmail.com>
2018-09-03 18:48:09 +0900

    started animateImages component

---
+++ src/components/animateImages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './index.css'
+
+class AnimateImages extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+    )
+  }
+}
+
+export default AnimateImages
