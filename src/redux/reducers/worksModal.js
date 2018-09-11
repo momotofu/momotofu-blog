70d8c38518fc7160673ddb528ffa332368e83910
Christopher Reece <mrkita.reece@gmail.com>
2018-09-11 14:22:43 +0900

    added worksModal reducer

---
+++ src/redux/reducers/worksModal.js
@@ -0,0 +1,18 @@
+import { TOGGLE_WORKS_MODAL } from '../constants/actionTypes'
+
+const initialState = {
+  isModalVisible: false
+}
+
+const worksModal = (state = initialState, action) => {
+  switch (action.type) {
+    case TOGGLE_WORKS_MODAL:
+      return object.assign({}, state, {
+        isModalVisible: !state.isModalVisible
+      })
+    default:
+      return state
+  }
+}
+
+export default worksModal
