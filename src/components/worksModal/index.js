c2368f2759e0417dea65ac77ca34a66f29a54404
Christopher Reece <mrkita.reece@gmail.com>
2018-09-19 13:35:35 +0900

    started WorksModal component

---
+++ src/components/worksModal/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './index.css'
+
+class WorksModal extends React.Component {
+  /**
+   * WorksModal
+   *
+   * state:
+   *
+   * component props are:
+   */
+
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default WorksModal
