6a8836a4926ecd7af78993d1fc0fbc764046670a
Christopher Reece <mrkita.reece@gmail.com>
2018-09-20 17:09:58 +0900

    forwad animation for WorksModal is working

--- src/components/worksModal/index.js
+++ src/components/worksModal/index.js
@@ -10,17 +10,75 @@ class WorksModal extends React.Component {
    * isShowing:
    * activeWorkID
    */
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isAnimating: false
+    }
+
+    this.anime = window.anime
+  }
+
+  animatePiecesForward(button, image, description) {
+    const duration = 1000
+
+    return this.anime.timeline({
+      loop: false,
+      autoplay: false
+    })
+      .add({
+        targets: description,
+        translateX: ['100%', '0%'],
+        easing: 'easeOutExpo',
+        duration,
+        offset: 0,
+        complete: (anim) => {
+          this.animateForward = this.animatePiecesForward(button, image, description)
+        }
+      })
+      .add({
+        targets: button,
+        translateX: ['-150%', '0%'],
+        easing: 'easeOutExpo',
+        duration,
+        offset: 0
+      })
+      .add({
+        targets: image,
+        translateY: ['150%', '0%'],
+        easing: 'easeOutExpo',
+        duration,
+        offset: 0,
+        complete: (anim) => {
+          console.log('completed')
+        }
+      })
+      .play
+  }
+
+  componentDidMount() {
+    this.worksModal = document.querySelector('.WorksModal')
+    const backButton = document.querySelector('#WorksModal-back-button')
+    const projectImage = document.querySelector('#WorksModal-project-image')
+    const descriptionPanel = document.querySelector('#WorksModal-description-panel')
+
+    this.animateForward = this.animatePiecesForward(backButton, projectImage, descriptionPanel)
+
+  }
 
   componentDidUpdate() {
     if (this.props.isShowing) {
       // prevent background from scrolling
       document.body.style.overflow = 'hidden'
       document.body.style.position = 'fixed'
+      this.animateForward()
     } else {
       // allow background to scroll again
       document.body.style.overflow = 'hidden'
       document.body.style.position = 'relative'
     }
+
   }
 
   exitButtonClickHandler() {
@@ -39,23 +97,31 @@ class WorksModal extends React.Component {
     })
   }
 
+
   render() {
     // return if no initial work is active
-    const work = this.props.work[0]
-    if (!work) return <div></div>
+    const work = this.props.work[0] ? this.props.work[0] : {
+      title: '',
+      description: '',
+      liveURL: '',
+      tags: []
+    }
 
     return (
-      <div className={ `WorksModal ${ this.props.isShowing ? '' : 'd-none' }` }>
+      <div className={ `WorksModal ${ this.props.isShowing && !this.state.isAnimating ? '' : 'd-none' }` }>
         <div className="WorksModal-bg"></div>
         <button
           className="WorksModal-button WorksModal-button-exit"
+          id="WorksModal-back-button"
           onClick={ this.exitButtonClickHandler.bind(this) }>
           <span className="underline">Back</span>
       </button>
-      <div className="WorksModal-panel">
-        <img className="WorksModal-panel-image" src={ process.env.PUBLIC_URL + '/images/under_cc.png' } />
+      <div className="WorksModal-panel" id="WorksModal-project-image">
+        <img className="WorksModal-panel-image" src={ process.env.PUBLIC_URL + '/images/under_cc.png' } alt="product" />
       </div>
-      <div className="WorksModal-panel WorksModal-panel-description">
+      <div
+        className="WorksModal-panel WorksModal-panel-description"
+        id="WorksModal-description-panel">
         <h1 className="WorksModal-panel-description-header">{ work.title }</h1>
         <p className="WorksModal-panel-description-paragraph">{ work.description }</p>
         <div className="WorksModal-cta">
